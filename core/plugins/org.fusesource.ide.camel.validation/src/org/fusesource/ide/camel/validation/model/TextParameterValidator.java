d48686876eb1bf1e3f6e336f9de8f4030904bb30
Lars Heinemann <lhein.smx@gmail.com>
2017-09-26 13:39:27 +0200

    FUSETOOLS-2394 - fixed sonar issues
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- core/plugins/org.fusesource.ide.camel.validation/src/org/fusesource/ide/camel/validation/model/TextParameterValidator.java
+++ core/plugins/org.fusesource.ide.camel.validation/src/org/fusesource/ide/camel/validation/model/TextParameterValidator.java
@@ -32,100 +32,133 @@ import org.fusesource.ide.foundation.core.util.Strings;
  */
 public final class TextParameterValidator implements IValidator {
 
-
 	private AbstractCamelModelElement camelModelElement;
 	private Parameter parameter;
+	private Object uriValue;
+	private Object refValue;
+	private CamelRouteContainerElement routeContainer;
 
 	public TextParameterValidator(AbstractCamelModelElement camelModelElement, Parameter parameter) {
 		this.camelModelElement = camelModelElement;
 		this.parameter = parameter;
+		this.uriValue = camelModelElement.getParameter("uri");
+		this.refValue = camelModelElement.getParameter("ref");
+		this.routeContainer = camelModelElement.getRouteContainer();
 	}
 
 	@Override
 	public IStatus validate(Object value) {
-		if (PropertiesUtils.isRequired(parameter) || "id".equalsIgnoreCase(parameter.getName())) {
-			final Object uriValue = camelModelElement.getParameter("uri");
-			final Object refValue = camelModelElement.getParameter("ref");
-			CamelRouteContainerElement routeContainer = camelModelElement.getRouteContainer();
-			if ("uri".equalsIgnoreCase(parameter.getName())) {
-				// only enforce URI if there is no REF set
-				if (Strings.isBlank((String) uriValue)) {
-					// no URI set -> check for REF
-					if (Strings.isBlank((String) refValue)) {
-						// there is no ref
-						return ValidationStatus.warning("One of Ref and Uri values have to be filled!");
-					} else {
-						// ref found - now check if REF has URI defined
-						AbstractCamelModelElement cme = routeContainer.findNode((String) refValue);
-						if (cme == null || cme.getParameter("uri") == null || ((String) cme.getParameter("uri")).trim().length() < 1) {
-							// no uri defined on ref
-							return ValidationStatus.warning("The referenced endpoint has no URI defined or does not exist.");
-						}
-					}
-				}
-
-				// check for broken refs
-				if (uriValue != null && ((String) uriValue).startsWith("ref:")) {
-					String refId = ((String) uriValue).trim().length() > "ref:".length() ? ((String) uriValue).substring("ref:".length()) : null;
-					List<String> refs = Arrays.asList(CamelComponentUtils.getRefs(camelModelElement.getCamelFile()));
-					if (refId == null || refId.trim().length() < 1 || !refs.contains(refId)) {
-						return ValidationStatus.warning("The entered reference does not exist in your context!");
-					}
-				}
-
-				// warn user if he set both ref and uri
-				if (uriValue != null && ((String) uriValue).trim().length() > 0
-						&& refValue != null && ((String) refValue).trim().length() > 0) {
-					return ValidationStatus.error("Please choose either URI or Ref but do not enter both values.");
-				}
-
-			} else if ("ref".equalsIgnoreCase(parameter.getName())) {
-
-				if (value != null && value instanceof String && value.toString().trim().length() > 0) {
-					String refId = (String) value;
-					AbstractCamelModelElement cme = routeContainer.findNode(refId);
-					if (cme == null) {
-						// check for global beans
-						if (!camelModelElement.getCamelFile().getGlobalDefinitions().containsKey(refId)) {
-							// the ref doesn't exist
-							return ValidationStatus.warning("The entered reference does not exist in your context!");
-						}
-					} else {
-						// the ref exists
-						if (cme.getParameter("uri") == null || ((String) cme.getParameter("uri")).trim().length() < 1) {
-							// but has no URI defined
-							return ValidationStatus.error("The referenced endpoint does not define a valid URI!");
-						}
-					}
-				}
-
-				// warn user if he set both ref and uri
-				if (uriValue != null && ((String) uriValue).trim().length() > 0
-						&& refValue != null && ((String) refValue).trim().length() > 0) {
-					return ValidationStatus.warning("Please choose only ONE of Uri and Ref.");
-				}
-
-			} else if ("id".equalsIgnoreCase(parameter.getName())) {
-				// check if ID is unique
-				if (value == null || value instanceof String == false || value.toString().trim().length() < 1) {
-					return ValidationStatus.warning("Parameter " + parameter.getName() + " is a mandatory field and cannot be empty.");
-				} else if (routeContainer != null && !routeContainer.isIDUnique((String) value)) {
-					return ValidationStatus.warning("Parameter " + parameter.getName() + " does not contain a unique value.");
-				} else {
-					Component component = PropertiesUtils.getComponentFor(camelModelElement);
-					if (component != null && value.equals(component.getScheme())) {
-						return ValidationStatus.error(NLS.bind(Messages.validationSameComponentIdAndComponentDefinitionId, parameter.getName(), value));
-					}
-				}
+		IStatus validationStatus = ValidationStatus.ok();
+		
+		if (isValidationRequired()) {
+			if (isUriParameter()) {
+				validationStatus = validateUri();
+			} else if (isRefParameter()) {
+				validationStatus = validateRef(value);
+			} else if (isIdParameter()) {
+				validationStatus = validateId(value);
 			} else {
-				// by default we only check for a value != null and
-				// length > 0
+				// by default we only check for a value != null and length > 0
 				if (value == null || value instanceof String && value.toString().trim().length() < 1) {
-					return ValidationStatus.warning("Parameter " + parameter.getName() + " is a mandatory field and cannot be empty.");
+					validationStatus =  ValidationStatus.warning("Parameter " + parameter.getName() + " is a mandatory field and cannot be empty.");
 				}
 			}
 		}
-		// all checks passed
+
+		return validationStatus;
+	}
+	
+	private boolean isValidationRequired() {
+		return PropertiesUtils.isRequired(parameter) || "id".equalsIgnoreCase(parameter.getName());
+	}
+	
+	private boolean isUriParameter() {
+		return "uri".equalsIgnoreCase(parameter.getName());
+	}
+	
+	private boolean isRefParameter() {
+		return "ref".equalsIgnoreCase(parameter.getName());
+	}
+	
+	private boolean isIdParameter() {
+		return "id".equalsIgnoreCase(parameter.getName());
+	}
+	
+	private boolean isRefAndUriEmpty() {
+		return Strings.isBlank((String) uriValue) && Strings.isBlank((String) refValue);
+	}
+	
+	private boolean isRefSet() {
+		return Strings.isBlank((String) uriValue) && !Strings.isBlank((String) refValue);
+	}
+	
+	private IStatus validateUri() {
+		// one of uri and ref has to be filled
+		if (isRefAndUriEmpty()) {
+			return ValidationStatus.warning("One of Ref and Uri values have to be filled!");
+		}
+		
+		// if ref then we need to check if uri defined
+		if (isRefSet()) {
+			// ref found - now check if REF has URI defined
+			AbstractCamelModelElement cme = routeContainer.findNode((String) refValue);
+			if (cme == null || cme.getParameter("uri") == null || ((String) cme.getParameter("uri")).trim().length() < 1) {
+				// no uri defined on ref
+				return ValidationStatus.warning("The referenced endpoint has no URI defined or does not exist.");
+			}
+		}
+
+		// check for broken refs
+		if (uriValue != null && ((String) uriValue).startsWith("ref:")) {
+			String refId = ((String) uriValue).trim().length() > "ref:".length() ? ((String) uriValue).substring("ref:".length()) : null;
+			List<String> refs = Arrays.asList(CamelComponentUtils.getRefs(camelModelElement.getCamelFile()));
+			if (refId == null || refId.trim().length() < 1 || !refs.contains(refId)) {
+				return ValidationStatus.warning("The entered reference does not exist in your context!");
+			}
+		}
+
+		// warn user if he set both ref and uri
+		if (uriValue != null && ((String) uriValue).trim().length() > 0	&& refValue != null && ((String) refValue).trim().length() > 0) {
+			return ValidationStatus.error("Please choose either URI or Ref but do not enter both values.");
+		}
+		
+		return ValidationStatus.ok();
+	}
+	
+	private IStatus validateRef(Object value) {
+		if (value instanceof String && ((String)value).trim().length() > 0) {
+			String refId = (String) value;
+			AbstractCamelModelElement cme = routeContainer.findNode(refId);
+			if (cme == null && !camelModelElement.getCamelFile().getGlobalDefinitions().containsKey(refId)) {
+				// the ref doesn't exist
+				return ValidationStatus.warning("The entered reference does not exist in your context!");
+			} else if (cme != null && cme.getParameter("uri") == null || ((String) cme.getParameter("uri")).trim().length() < 1) {
+				// but has no URI defined
+				return ValidationStatus.error("The referenced endpoint does not define a valid URI!");
+			}
+		}
+
+		// warn user if he set both ref and uri
+		if (uriValue != null && ((String) uriValue).trim().length() > 0 && refValue != null && ((String) refValue).trim().length() > 0) {
+			return ValidationStatus.warning("Please choose only ONE of Uri and Ref.");
+		}
+		
+		return ValidationStatus.ok();
+	}
+	
+	private IStatus validateId(Object value) {
+		// check if ID is unique
+		if (value == null || value instanceof String == false || value.toString().trim().length() < 1) {
+			return ValidationStatus.warning("Parameter " + parameter.getName() + " is a mandatory field and cannot be empty.");
+		} else if (routeContainer != null && !routeContainer.isIDUnique((String) value)) {
+			return ValidationStatus.warning("Parameter " + parameter.getName() + " does not contain a unique value.");
+		} else {
+			Component component = PropertiesUtils.getComponentFor(camelModelElement);
+			if (component != null && value.equals(component.getScheme())) {
+				return ValidationStatus.error(NLS.bind(Messages.validationSameComponentIdAndComponentDefinitionId, parameter.getName(), value));
+			}
+		}
+		
 		return ValidationStatus.ok();
 	}
 }
